fcbb58f2b6803ac3259fe7231ba71b8df21acb1e
Tova Holmes <tova.r.holmes@gmail.com>
2026-03-20 19:35:11 -0500

    Write explicit calibrated comparison cluster collection

--- k4Reco/steer_reco.py
+++ k4Reco/steer_reco.py
@@ -1091,6 +1091,7 @@ DDMarlinPandora.Parameters = {
 }
 
 if the_args.writeClusterCalibrationComparison:
+    DDMarlinPandora.Parameters["CalibratedComparisonClusterCollectionName"] = ["PandoraClustersCalibrated"]
     DDMarlinPandora.Parameters["UncalibratedClusterCollectionName"] = ["PandoraClustersRaw"]
 
 if the_args.thetaEnergyCalibPayload:
